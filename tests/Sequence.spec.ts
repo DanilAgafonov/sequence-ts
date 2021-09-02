67f3be5dfd64c7403d1bd83f42b44252dca38c98
Danil Agafonov <danil.agafonov@flatstack.com>
2021-09-02 02:12:27 +0200

    Init

---
+++ tests/Sequence.spec.ts
@@ -0,0 +1,33 @@
+import { Sequence } from "../src/Sequence";
+
+describe("Sequence", () => {
+  let sequence: Sequence;
+
+  beforeEach(() => {
+    sequence = new Sequence("1");
+  });
+
+  describe("#toString", () => {
+    it("returns initial value", () => {
+      expect(String(sequence)).toBe("1");
+    });
+  });
+
+  describe("#next", () => {
+    it("generates next state", () => {
+      expect(String(sequence.next())).toBe("11");
+    });
+
+    it("returns instance of Sequence", () => {
+      expect(sequence.next()).toBeInstanceOf(Sequence);
+    });
+
+    it("generates next state 2 times", () => {
+      expect(String(sequence.next().next())).toBe("21");
+    });
+
+    it("generates next state 3 times", () => {
+      expect(String(sequence.next().next().next())).toBe("1211");
+    });
+  });
+});
